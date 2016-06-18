e0cc75b0981ba45356a645374bbb15f36b8ce1e9
MiffyLiye <miffyliye@gmail.com>
2016-06-18 19:34:45 +0800

    Update output information format

--- TcpPing/Program.cs
+++ TcpPing/Program.cs
@@ -37,7 +37,7 @@ namespace TcpPing
 
             var endPoint = new IPEndPoint(ip, port);
 
-            Console.WriteLine($"Pinging {hostname}:{port} [{ip}:{port}]:");
+            Console.WriteLine($"TCP connect to {hostname}:{port} [{ip}:{port}]:");
 
             TcpPing(endPoint, WarmUpLimit);
             var pingResults = new List<double?>();
@@ -45,41 +45,41 @@ namespace TcpPing
             {
                 var delay = TcpPing(endPoint, TimeOutLimit);
                 pingResults.Add(delay);
-                Console.WriteLine(delay.HasValue
-                                      ? $"time = {delay.Value:0.00} ms"
-                                      : "Request timed out");
+                Console.WriteLine(
+                    delay.HasValue
+                        ? $"Connecting to {ip}:{port}: time = {delay.Value:0.00} ms"
+                        : "Request timed out");
                 Thread.Sleep(RetryInterval);
             }
 
-            Console.WriteLine($"--- {ip}:{port} ping statistics ---");
-
-            var validDelays = pingResults
-                .Where(t => t.HasValue)
-                .Select(t => t.Value)
-                .ToList();
-            if (validDelays.Any())
-                Console.WriteLine(
-                    $"round-trip min/avg/max = {validDelays.Min():0.00}/{validDelays.Max():0.00}/{validDelays.Average():0.00} ms");
+            Console.WriteLine();
+            Console.WriteLine($"TCP connect statistics for {ip}:{port}:");
 
             var sentCount = pingResults.Count;
             var receivedCount = pingResults.Count(t => t.HasValue);
             var lossCount = sentCount - receivedCount;
             var lossRate = lossCount / sentCount;
-            if (lossCount > 0)
-                Console.WriteLine(
-                    $"{sentCount} packets transmitted, {receivedCount} packets received, {100 * lossRate:0.0}% packet loss");
+            Console.Write($"Sent = {sentCount}, ");
+            Console.Write($"Received = {receivedCount}, ");
+            Console.Write($"Loss = {lossCount} ");
+            Console.WriteLine($"({100 * lossRate:0}% loss)");
+
+            if (!pingResults.Any(t => t.HasValue)) return;
+
+            var validDelays = pingResults.Where(t => t.HasValue).Select(t => t.Value).ToList();
+            Console.Write($"Minimum = {validDelays.Min():0.00} ms, ");
+            Console.Write($"Maximum = {validDelays.Max():0.00} ms, ");
+            Console.WriteLine($"Average = {validDelays.Average():0.00} ms");
         }
 
         // ReSharper disable once SuggestBaseTypeForParameter
         private static double? TcpPing(IPEndPoint endPoint, TimeSpan timeOutLimit)
         {
             using (
-                var socket =
-                    new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-                    {
-                        Blocking = true
-                    }
-            )
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                {
+                    Blocking = true
+                })
             {
                 var stopWatch = new Stopwatch();
 
